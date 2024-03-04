90dda0db1baf5740d6408bb7e14df18261515055
mayankchugh1977 <mayank.chugh@gmail.com>
2024-03-04 12:57:20 +0800

    addiotnal files

---
+++ addtional code/regex_soup.py
@@ -0,0 +1,14 @@
+import re
+from urllib.request import urlopen
+
+#url = "http://olympus.realpython.org/profiles/dionysus"
+url = "https://www.flipkart.com"
+page = urlopen(url)
+html = page.read().decode("utf-8")
+
+pattern = "<title.*?>.*?</title.*?>"
+match_results = re.search(pattern, html, re.IGNORECASE)
+title = match_results.group()
+title = re.sub("<.*?>", "", title)  # Remove HTML tags
+
+print(title)
